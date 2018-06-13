1b0a7a34581c79f8de69fd9d16e6ce0493d38589
czarnini <mboguck2@elka.pw.edu.pl>
2018-06-13 18:34:16 +0200

    firstTasks

--- src/i_introduction/_1_Java_To_Kotlin_Converter/n01JavaToKotlinConverter.kt
+++ src/i_introduction/_1_Java_To_Kotlin_Converter/n01JavaToKotlinConverter.kt
@@ -5,7 +5,8 @@ import util.TODO
 fun todoTask1(collection: Collection<Int>): Nothing = TODO(
     """
         Task 1.
-        Convert the Java method 'task1' of the class 'JavaCode1' into Kotlin.
+        Convert the Java method
+        'task1' of the class 'JavaCode1' into Kotlin.
         In IntelliJ IDEA or Android Studio, you can copy the Java code,
         paste it into the Kotlin file and let IDE convert it.
         Please use automatic conversion for this task only.
@@ -14,5 +15,16 @@ fun todoTask1(collection: Collection<Int>): Nothing = TODO(
 
 
 fun task1(collection: Collection<Int>): String {
-    todoTask1(collection)
+    val sb = StringBuilder()
+    sb.append("{")
+    val iterator = collection.iterator()
+    while (iterator.hasNext()) {
+        val element = iterator.next()
+        sb.append(element)
+        if (iterator.hasNext()) {
+            sb.append(", ")
+        }
+    }
+    sb.append("}")
+    return sb.toString()
 }
